18fc67dbfb3cfaa66ff4fe53ee9a7877e9c6590a
Oran Alster <117355063+Oran01@users.noreply.github.com>
2025-03-20 20:17:44 +0200

    Delete .next directory

--- .next/server/flight-server-css-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_CSS_MANIFEST={"__entry_css__":{"C:\\Users\\User\\Desktop\\Projects\\metaverse\\app\\page":["C:\\Users\\User\\Desktop\\Projects\\metaverse\\styles\\globals.css"]},"C:\\Users\\User\\Desktop\\Projects\\metaverse\\app\\layout.js":["C:\\Users\\User\\Desktop\\Projects\\metaverse\\styles\\globals.css"]}
